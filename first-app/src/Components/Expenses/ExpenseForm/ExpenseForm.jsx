dfd392d85cef63b146c70044558baae2312a67d3
Sumit Khandelwal <synergy2411@outlook.com>
2025-12-09 11:56:18 +0530

    expense form added

--- first-app/src/Components/Expenses/ExpenseForm/ExpenseForm.jsx
+++ first-app/src/Components/Expenses/ExpenseForm/ExpenseForm.jsx
@@ -1,5 +1,70 @@
 function ExpenseForm() {
-  return <h1>Form coming soon...</h1>;
+  return (
+    <div className="row">
+      <div className="col-8 offset-2">
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <h4 className="text-center">Add Your Expense</h4>
+              {/* title */}
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  id="title"
+                  placeholder=""
+                />
+                <label htmlFor="title">Title</label>
+              </div>
+
+              {/* amount */}
+              <div className="form-floating mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  name="amount"
+                  id="amount"
+                  placeholder=""
+                  step="0.5"
+                  min="0.0"
+                />
+                <label htmlFor="amount">Amount</label>
+              </div>
+
+              {/* createdAt */}
+              <div className="form-floating mb-3">
+                <input
+                  type="date"
+                  className="form-control"
+                  name="createdAt"
+                  id="createdAt"
+                  placeholder=""
+                  min="2023-04-01"
+                  max="2026-03-31"
+                />
+                <label htmlFor="createdAt">Created Date</label>
+              </div>
+
+              {/* buttons */}
+              <div className="row">
+                <div className="col">
+                  <div className="d-grid">
+                    <button className="btn btn-primary">Add</button>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="d-grid">
+                    <button className="btn btn-secondary">Close</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseForm;
